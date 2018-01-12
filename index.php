41b734c1429c3f6506d6735972d16c75ba489aea
khoshina <khoshina@nttr.co.jp>
2018-01-12 15:33:29 +0900

    change：HTMLファイルからPHPファイルに変更。

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	$pagetitle = 'Hello World!!';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title><?php echo $pagetitle; ?></title>
+</head>
+<body>
+<h1><?php echo $pagetitle; ?></h1>
+</body>
+</html>
